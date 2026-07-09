8c6f1676fb3e36ea371c10f08a29b8557641f108
Bhoomi Sharma <bhoomisharma404@gmail.com>
2026-07-09 14:20:13 +0530

    rainbowkit setup

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -4,13 +4,16 @@ import { WagmiProvider } from "wagmi";
 import { config } from "../utils/wagmiConfig";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
     </WagmiProvider>
   );
 }
--- frontend/utils/wagmiConfig.ts
+++ frontend/utils/wagmiConfig.ts
@@ -1,10 +1,14 @@
-import { createConfig, http } from "wagmi";
+import { http } from "wagmi";
 import { sepolia, hardhat } from "wagmi/chains";
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 
-export const config = createConfig({
+export const config = getDefaultConfig({
+  appName: "CoSign",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? "",
   chains: [hardhat, sepolia],
   transports: {
-    [hardhat.id]: http(),
-    [sepolia.id]: http(),
+    [hardhat.id]: http("http://127.0.0.1:8545/"),
+    [sepolia.id]: http(process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL),
   },
+  ssr: true,
 });
