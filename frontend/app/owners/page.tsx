6f68ce8f3722feec502a03acef040c1a5c206d2a
Bhoomi Sharma <bhoomisharma404@gmail.com>
2026-07-12 10:34:35 +0530

    fixed stuff

--- frontend/app/owners/page.tsx
+++ frontend/app/owners/page.tsx
@@ -1,5 +1,6 @@
-import SideBar from "../components/SideBar";
-export default function Home() {
+import SideBar from "@/components/SideBar";
+
+export default function Owners() {
   return (
     <div className="h-screen w-screen bg-(--color-bg) dark:bg-(--color-bg)">
       <SideBar />
